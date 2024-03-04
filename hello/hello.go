47ebb02bebd6faabf6742db37314837204f0f22f
Shiwen An <sweynan@icloud.com>
2024-03-04 12:00:17 +0900

    update test function

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
-	fmt.Println(quote.Go())
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
